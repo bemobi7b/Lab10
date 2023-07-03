ef9363a00813672415b94c3600065a01ae8bba00
Bennett <bemobi7b@gmail.com>
2023-07-03 11:01:06 -0500

    Final

---
+++ src/Lab10.ino
@@ -0,0 +1,46 @@
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+
+#define LEDPIN D5
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.isConnected();
+  Serial.available();
+  while (!Serial.isConnected())
+  {
+  }
+  Serial1.begin(9600);
+  // Serial1.isConnected();
+  Serial1.available();
+  // while(!Serial1.isConnected()){}
+  pinMode(LEDPIN, OUTPUT);
+}
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    char value = Serial.read();
+    // byte byteValue = value;
+    Serial.println(value);
+    Serial1.print(value);
+  }
+  if (Serial1.available() > 0)
+  {
+    byte byteNewValue = Serial1.read();
+    Serial.println(byteNewValue);
+    if (byteNewValue == 48)
+    {
+      digitalWrite(LEDPIN, LOW);
+    }
+    if (byteNewValue == 49)
+    {
+      digitalWrite(LEDPIN, HIGH);
+    }
+    if (byteNewValue == 88)
+    {
+      Serial.println("Wrong button, dummy.");
+    }
+  }
+}
